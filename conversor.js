5da56e1e79675ea21bd632ad0bb3dd7d36451516
SantiagoCoral09 <santiagocoral80@gmail.com>
2023-06-25 17:36:14 -0500

    Conversor de Presion de Pascal a Bar

--- conversor.js
+++ conversor.js
@@ -1,3 +1,4 @@
+///Conversor de temperatura celsius a Fahrenheit
 let celsius = document.getElementById("celsius");
 let fahrenheight = document.getElementById("fahrenheight");
 
@@ -33,3 +34,33 @@ fahrenheight.addEventListener(
 
 const convertToFahrenheight = (celsius) => (Number(celsius) * 9) / 5 + 32;
 const convertToCelsius = (celsius) => ((Number(celsius) - 32) * 5) / 9;
+
+////Conversor de Presion de Pascal a bar
+let pascal = document.getElementById("pascal");
+let bar = document.getElementById("bar");
+
+pascal.addEventListener(
+  "input",
+  debounce(() => {
+    if (pascal.value.length != 0) {
+      bar.value = convertToBar(pascal.value);
+    } else {
+      bar.value = "";
+    }
+  }, 500)
+);
+
+bar.addEventListener(
+  "input",
+  debounce(() => {
+    if (bar.value.length != 0) {
+      pascal.value = convertToPascal(bar.value);
+    } else {
+      pascal.value = "";
+    }
+  }, 500)
+);
+
+const convertToBar = (valor) => (Number(valor) /100000);
+const convertToPascal = (valor) => ((Number(valor) *100000));
+
